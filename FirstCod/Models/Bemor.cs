844d01a46a4b6319a1c15180d5e189434e3a977b
user <user@WIN-5US979F84Q9>
2024-02-21 22:06:13 +0500

    Добавьте файлы проекта.

---
+++ FirstCod/Models/Bemor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCod.Models
+{
+    internal class Bemor
+    {
+        public int Id { get; set; }
+        public string Ism { get; set; }
+        public string Familiya { get; set; }
+        public string KasallikTuri { get; set; }
+    }
+}
---
+++ FirstCod/Program.cs
@@ -0,0 +1,19 @@
+﻿using FirstCod.Models;
+
+using (ApplicationDbContext db=new ApplicationDbContext())
+{
+    Bemor bemor1 = new Bemor { Ism = "Otabek", Familiya = "Muhiddinov", KasallikTuri = "Ko'p tashqariga chiqadi" };
+    Bemor bemor2 = new Bemor { Ism = "Ali", Familiya = "Sobirov", KasallikTuri = "Shomollash" };
+
+    db.Bemorlar.AddRange(bemor1, bemor2);
+    db.SaveChanges();
+    Console.WriteLine("Bazaga Saqlandi");
+
+
+    var bemorlar = db.Bemorlar.ToList();
+
+    foreach (var i in bemorlar)
+    {
+        Console.WriteLine($"{i.Id}.{i.Ism} {i.Familiya}  {i.KasallikTuri}");
+    }
+}
